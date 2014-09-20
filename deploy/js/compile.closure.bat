eec17284f2da21c72d1367156fe777d985cd80e9
Nicholas Archer <the.nicholas.archer@gmail.com>
2014-09-20 22:58:21 +0800

    0.5 hr tidy and compress

--- deploy/js/compile.closure.bat
+++ deploy/js/compile.closure.bat
@@ -4,7 +4,7 @@
 
 
 
-"Y:\Program Files (x86)\Java\jre7\bin\java.exe" -jar "U:\Nicholas Archer\Apps\google_compiler.jar"   --compilation_level SIMPLE    --js_output_file="out\main.js"  calc\**.js *.js
+"Y:\Program Files (x86)\Java\jre7\bin\java.exe" -jar "U:\Nicholas Archer\Apps\google_compiler.jar"   --compilation_level SIMPLE    --js_output_file="out\main.js"  calc\**.js *.js !*Require.js
 
 
 
